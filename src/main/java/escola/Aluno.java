2919f4021a7c08812cdb4a24376da113a2c39562
Josimar Minete <josimaminete@gmail.com>
2022-03-15 20:52:12 -0300

    Adiciona class que representa
    o Aluno

---
+++ src/main/java/escola/Aluno.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Aluno {
+	
+	//Entidade
+	//Jpa = O espelho do banco de dados o dominio
+	//Celan Arquitecture = Camada de regra de negócio
+	//Classe que tenha um atributo que identifique unicamente o objeto.
+
+	private String cpf;
+	private String nome;
+	private String email;
+	
+}
