a3083a4dbced0ea9a6f6ad3e8bf97c2d4741559a
Josimar Minete <josimaminete@gmail.com>
2022-05-03 22:51:52 -0300

    Correções

--- src/main/java/escola/Email.java
+++ src/main/java/escola/Email.java
@@ -9,10 +9,10 @@ public class Email {
 		if (endereco == null) {
 			throw new IllegalArgumentException("Informe o E-mail!");
 		}
-		//"^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$\""
-		//"^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$"
-		//"(\\\\S.*\\\\S)(@)(\\\\S.*\\\\S)(.\\\\S[a-z]{2,3})"
-		//"(\\S.*\\S)(@)(\\S.*\\S)(.\\S[a-z]{2,3})"
+		// "^[\\\\w\\\\.-]+@([\\\\w\\\\-]+\\\\.)+[A-Z]{2,4}$\""
+		// "^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$"
+		// "(\\\\S.*\\\\S)(@)(\\\\S.*\\\\S)(.\\\\S[a-z]{2,3})"
+		// "(\\S.*\\S)(@)(\\S.*\\S)(.\\S[a-z]{2,3})"
 		if (!endereco.matches("(\\S.*\\S)(@)(\\S.*\\S)(.\\S[a-z]{2,3})")) {
 			throw new IllegalArgumentException("E-mail inválido!");
 		}
